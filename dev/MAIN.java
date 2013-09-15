c8df405188723dca89443127865a8b3c32149df2
Markus B. <boerner.markus@gmail.com>
2013-09-16 01:24:13 +0200

    Create MAIN.java

---
+++ dev/MAIN.java
@@ -0,0 +1,30 @@
+
+public class MAIN
+{
+    /**
+     * Constructor for objects of class MAIN
+     */
+    public MAIN(String args)
+    {
+        //do something with start params
+        ;
+        
+        //create a thread for the settings management class and (re-)reading the cfg/ini
+        ;
+        
+        //create a thread of main socket with param: settings clss
+        ;
+        
+        //catch exceptions somathing may have an error->dump informations in ./log/crash
+        ;
+        
+    }
+
+    /*
+     * program entry
+     */
+    public static void main(String args)
+    {
+        MAIN main = new MAIN(args);
+    }
+}
