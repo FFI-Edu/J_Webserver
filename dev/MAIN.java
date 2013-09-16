f47f5b19794b4fab9edd1714cbcb24471c9a4606
Markus B. <boerner.markus@gmail.com>
2013-09-16 21:33:59 +0200

    Update MAIN.java

--- dev/MAIN.java
+++ dev/MAIN.java
@@ -1,26 +1,35 @@
 
 public class MAIN
 {
+    SETTINGS set;
+    SERVER server;
+    
+    
     /**
      * Constructor for objects of class MAIN
      */
     public MAIN(String args)
     {
+        LOG.write("entering main tread with params: "+args.toString() );
         //do something with start params
         ;
+        try{
+            //create object for the settings management class and (re-)reading the cfg/ini
+            set=new SETTINGS();
         
-        //create a thread for the settings management class and (re-)reading the cfg/ini
-        ;
-        
-        //create a thread of main socket with param: settings clss
-        ;
-        
-        //catch exceptions somathing may have an error->dump informations in ./log/crash
-        ;
-        
+            //create a thread of main socket with param: settings clss
+            server=new SERVER(set);
+        }catch (Exception e){
+            //catch exceptions somathing may have an error->dump informations in ./log/crash
+            
+        }
+        finally{
+            
+            ;
+        }
     }
 
-    /*
+    /**
      * program entry
      */
     public static void main(String args)
