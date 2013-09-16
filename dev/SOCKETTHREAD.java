a81ba8a7bdd46e25197b135dd1031b577d59fa28
Markus B. <boerner.markus@gmail.com>
2013-09-17 01:42:30 +0200

    Create SOCKETTHREAD.java

---
+++ dev/SOCKETTHREAD.java
@@ -0,0 +1,25 @@
+import java.net.*;
+
+//System.out.println ( conv.encode(":/éäöüß", "UTF-8")+"éäöüß" );
+
+public class SOCKETTHREAD extends Thread
+{
+    private int arrayPosition;
+    private SERVER server;
+    private SETTINGS set;
+
+    /**
+     * Constructor for objects of class SOCKETTHREAD
+     */
+    public SOCKETTHREAD(int arrPos, Socket socket, SERVER serv, SETTINGS sett)
+    {
+        int arrayPosition;
+        SERVER server;
+        SETTINGS set;
+    }
+    
+    public void sampleMethod(int y)
+    {
+        ;
+    }
+}
