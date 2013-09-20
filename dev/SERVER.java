8eb56a09497bb1c1ceb408f953f2fc61e55f494a
Markus B. <boerner.markus@gmail.com>
2013-09-20 21:02:52 +0200

    Update SERVER.java

--- dev/SERVER.java
+++ dev/SERVER.java
@@ -6,20 +6,18 @@ import java.lang.*;
 
 public class SERVER
 {
-    Socket client;
-    String nachricht;
     ServerSocket serverSocket;
-    URLEncoder conv;
-    
     SETTINGS set;
     
     Thread [] activeThreads;
     
-    int port = 1234;
-    
     public SERVER(SETTINGS s) throws IOException
     {
+        set=s;
         activeThreads = new Thread [ set.getMaxThreads() ] ;
+        for(int i=0;i<set.getMaxThreads();i++){
+            activeThreads[i]=null;
+        }
         serverSocket = new ServerSocket( set.getPort() );
         try {
             startAccept();
@@ -32,7 +30,14 @@ public class SERVER
         LOG.write("Ready to accept on "+set.getPort());
         while(true){
             try {
-                accept ( serverSocket.accept() );
+                int firstEmpty=firstEmptySlot();
+                if(firstEmpty>=0){
+                    activeThreads [ firstEmpty ] = new Thread( new SOCKETTHREAD(firstEmpty, serverSocket.accept() , this, set) ) ;
+                    
+                }else{
+                    LOG.write("Max Socketthreads reched! No more space in Array! Closing socket.",2);
+                    serverSocket.accept().close();
+                }
             } catch (IOException e) {
                 LOG.write("Err while accepting a socket.",1);
                 e.printStackTrace();
@@ -40,63 +45,23 @@ public class SERVER
         }
     }
     
-    private void accept(Socket socket) throws IOException {
-        activeThreads [ firstEmptySlot() ] = new Thread( new SOCKETTHREAD(firstEmptySlot(), socket, this, set) );
-    }
-    
     private int firstEmptySlot(){
-        return 0;
+        for(int i=0; i<activeThreads.length ; i++){
+            if( activeThreads[i] == null || !activeThreads[i].isAlive()  ){
+                return i;
+            }
+        }
+        return -1;
     }
     
-    private String verarbeiten(Socket socket) throws IOException {
-        int buffersize = socket.getReceiveBufferSize();
-        //System.out.println("Buffersize: "+buffersize+"; ");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        /*char[] buffer = new char[buffersize];
-        int anzahlZeichen = bufferedReader.read(buffer, 0, buffersize);
-        String nachricht = new String(buffer, 0, anzahlZeichen);*/
-        String nachricht="";
-        String inputLine;
-        int dg=0;        
-        while ((inputLine = bufferedReader.readLine()) != null){
-            //System.out.println(dg+"");
-            nachricht += (inputLine+"\n");
-            //System.out.println(dg+"1");
-            dg++;
-            if(dg==15){break;}
-        }   
-        System.out.println("out");        
-        schreibeNachricht(socket);
-        bufferedReader.close();
-        socket.close();        
-        System.out.println(nachricht);
-        return nachricht;
-    }
-    
-    private void schreibeNachricht(Socket socket) throws IOException {
-        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    public void threadFinished( int position){
         
-        /*try {
-           BufferedReader in = new BufferedReader(new FileReader("test.html"));
-           String zeile = null;
-           int n = 0;
-          while ((zeile = in.readLine()) != null) {
-            //System.out.println("Gelesene Zeile: " + zeile);
-            printWriter.println(zeile);
-          }
-        } catch (IOException e) {
-             e.printStackTrace();
-        }*/
-          
-        printWriter.println("Hello,l1");
-        printWriter.println("Bye,l2");
-        printWriter.flush();
-        
-        
-       
-        //System.out.println("#Client abgeshclossen ");
-        
-        
+        try{
+            activeThreads [ position ].interrupt();
+        }catch(Exception e){;}
+        activeThreads [ position ] = null;
+        System.gc ();
+        LOG.write("Arrayposition "+position+" finished job");
     }
     
 }
