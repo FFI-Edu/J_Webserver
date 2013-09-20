770766e838733fd3bb5990df9cd0a94dc90763db
Markus B. <boerner.markus@gmail.com>
2013-09-20 21:03:05 +0200

    Update SOCKETTHREAD.java

--- dev/SOCKETTHREAD.java
+++ dev/SOCKETTHREAD.java
@@ -1,25 +1,90 @@
 import java.net.*;
+import java.io.*;
+import java.lang.*;
 
 //System.out.println ( conv.encode(":/éäöüß", "UTF-8")+"éäöüß" );
 
-public class SOCKETTHREAD extends Thread
+public class SOCKETTHREAD implements Runnable
 {
     private int arrayPosition;
-    private SERVER server;
     private SETTINGS set;
+    private Socket socket;
+    private SERVER server;
+    private long starttime;
+    
 
     /**
      * Constructor for objects of class SOCKETTHREAD
      */
-    public SOCKETTHREAD(int arrPos, Socket socket, SERVER serv, SETTINGS sett)
+    public SOCKETTHREAD(int arrPos, Socket sock, SERVER serv, SETTINGS sett)
     {
-        int arrayPosition;
-        SERVER server;
-        SETTINGS set;
+        //LOG.write("New Sockettread in pos "+arrPos);
+        arrayPosition   = arrPos;
+        set             = sett;
+        socket          = sock;
+        
+        new Thread( this ).start();
     }
     
-    public void sampleMethod(int y)
+    public void run(){
+        starttime = System.currentTimeMillis();
+        try{
+            in();
+        }catch(Exception e){
+            LOG.write("Error communicating with the client. "+e.toString(),3);
+        }finally{
+            close(server);
+        }
+    }
+    
+    public void in() throws Exception
     {
-        ;
+        int buffersize = socket.getReceiveBufferSize();
+        BufferedReader bufferedReader = 
+            new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()));
+        char[] buffer = new char[buffersize];
+        int anzahlZeichen = bufferedReader.read(buffer, 0, buffersize); // blockiert bis Nachricht empfangen
+        String request = new String(buffer, 0, anzahlZeichen);
+        
+        System.out.println(request);
+        
+        out(request);
+    }
+    public void out(String request) throws Exception
+    {
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        
+        printWriter.println("Hello,l1");
+        new Thread( this ).sleep(1000);
+        printWriter.println("Bye,l2");
+        
+        printWriter.flush();
+        
+    }
+    
+    private void close(SERVER serv){
+        try{
+            socket.close();
+            socket=null;
+            new Thread( this ).interrupt();
+            //LOG.write("if you see this Java is shit");
+        }catch(Exception e){;}
+        
+    }
+    
+    public boolean isAlive(){
+        if(socket==null)
+            return false;
+        if(System.currentTimeMillis()-starttime > set.getMaxThreadtime())
+            return false;
+        
+        return true;
+    }
+        
+    
+    public String toString(){
+        return "ThreadOn"+arrayPosition;
     }
 }
