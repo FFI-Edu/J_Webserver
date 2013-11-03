88a21974eb724a29d64d0736c4831f06047f9a66
Markus B. <boerner.markus@gmail.com>
2013-11-03 03:31:00 +0100

    Create REQUESTHANDLER.java

---
+++ dev/REQUESTHANDLER.java
@@ -0,0 +1,13 @@
+
+public class REQUESTHANDLER
+{
+    public REQUESTHANDLER()
+    {
+        
+    }
+    
+    public byte[] getOutputBytes()
+    {
+        return "HTTP/1.0 501 Not Implemented".getBytes();
+    }
+}
